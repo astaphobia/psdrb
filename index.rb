e018fc112d4fccc4646750e72506cc5e34d60f4a
astaphobia <ariasta.ketut@icloud.com>
2018-01-25 09:32:16 +0800

    ♻️ mkdir and download file psd

--- index.rb
+++ index.rb
@@ -1,13 +1,31 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/namespace'
 require 'psd'
+require 'fileutils'
+require 'open-uri'
 
 set :port => 8080
 configure :production do
   enable :reloader
 end
 
-get '/upload-psd' do
-    path = './assets/temp_files/'
-    psd = PSD.open(path + request.body)
+
+get '/' do
+    "Hello Guys, ruby here :)"
+end
+    
+post '/api/upload-psd' do
+    directory = './asset/temp_files/'
+    url = params['filepath']
+    filepath = [directory, params['filename']].join()
+
+    FileUtils::mkdir_p directory
+
+    File.open(filepath, "wb") do |file|
+      file.write open(url).read
+    end
+    psd = PSD.new(filepath)
+    psd.parse!
+    print psd
 end
